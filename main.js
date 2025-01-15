1f587bab7b3641e256d4427c32fb71b96ce24a4b
Dev-RiQ <lwk525678@gmail.com>
2025-01-15 17:02:15 +0900

    feat: closed #1

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const num2 = 20;
 
 function addNums(num1, num2){
     console.log(`${num1} + ${num2} = ${num1+num2}`);
-    console.log("기능 완료");
+    console.log("기능 완료 !");
 }
 
 addNums(num1, num2);
